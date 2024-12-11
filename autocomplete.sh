fa58ebb98cc085b6f2a47a43cd9b4a2bbc292493
Marc Villacorta <h0tbird@github.com>
2024-12-11 13:29:59 +0100

    cilium completion

--- autocomplete.sh
+++ autocomplete.sh
@@ -4,3 +4,5 @@
 source <(kubectl completion zsh) && compdef _kubectl kubectl
 source <(helm completion zsh) && compdef _helm helm
 source <(kind completion zsh) && compdef _kind kind
+source <(cilium completion zsh) && compdef _cilium cilium
+
