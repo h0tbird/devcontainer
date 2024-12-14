7bf66e0af8d1ee20cc1372bd4ed4b83ac768d0be
Marc Villacorta <h0tbird@github.com>
2024-12-15 00:37:06 +0100

    Remove autocomplete.sh

--- autocomplete.sh
+++
@@ -1,9 +0,0 @@
-
-# Completion
-
-source <(kubectl completion zsh) && compdef _kubectl kubectl
-source <(helm completion zsh) && compdef _helm helm
-source <(kind completion zsh) && compdef _kind kind
-source <(cilium completion zsh) && compdef _cilium cilium
-source <(argocd completion zsh) && compdef _argocd argocd
-source <(argo completion zsh) && compdef _argo argo
