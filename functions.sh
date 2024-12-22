bc164ff38b029a2ba93d42efbcb6b98d57fd7bbb
Marc Villacorta <h0tbird@github.com>
2024-12-22 18:50:49 +0100

    Update mlw

--- functions.sh
+++ functions.sh
@@ -2,10 +2,14 @@
 # MeshLab Watch
 function mlw {
   watch '
-    k --context kind-kube-00 get po,svc -A;
+    k --context kind-kube-00 get po -A --field-selector=status.phase!=Succeeded; echo
+    k --context kind-kube-00 get svc -A --field-selector=spec.type=LoadBalancer;
     echo "\n-------\n";
-    k --context kind-pasta-1 get po,svc -A;
+    k --context kind-pasta-1 get po -A --field-selector=status.phase!=Succeeded; echo
+    k --context kind-pasta-1 get svc -A --field-selector=spec.type=LoadBalancer;
     echo "\n-------\n";
-    k --context kind-pasta-2 get po,svc -A
+    k --context kind-pasta-2 get po -A --field-selector=status.phase!=Succeeded; echo
+    k --context kind-pasta-2 get svc -A --field-selector=spec.type=LoadBalancer;
   '
 }
+
