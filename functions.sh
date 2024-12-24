4de75acf68927bfd947172183866d1b002ae1746
Marc Villacorta <h0tbird@github.com>
2024-12-24 11:06:43 +0100

    Introducing blue

--- functions.sh
+++ functions.sh
@@ -1,19 +1,24 @@
 
+# Blue
+function blue {
+  echo -e "\n\e[1;36m$1\e[0m\n"
+}
+
 # MeshLab
 function ml {
-    echo "\n--[ Manager ]---------------------------------------------------\n";
+    blue "--[ Manager ]-------------------------------------------------------";
     k --context kind-mnger-1 get po -A --field-selector=status.phase!=Succeeded; echo
     k --context kind-mnger-1 get svc -A --field-selector=spec.type=LoadBalancer;
-    echo "\n--[ Pizza ]-----------------------------------------------------\n";
+    blue "--[ Pizza ]---------------------------------------------------------";
     k --context kind-pizza-1 get po -A --field-selector=status.phase!=Succeeded; echo
     k --context kind-pizza-1 get svc -A --field-selector=spec.type=LoadBalancer;
-    echo "\n-------\n";
+    blue "-------";
     k --context kind-pizza-2 get po -A --field-selector=status.phase!=Succeeded; echo
     k --context kind-pizza-2 get svc -A --field-selector=spec.type=LoadBalancer;
-    echo "\n--[ Pasta ]-----------------------------------------------------\n";
+    blue "--[ Pasta ]---------------------------------------------------------";
     k --context kind-pasta-1 get po -A --field-selector=status.phase!=Succeeded; echo
     k --context kind-pasta-1 get svc -A --field-selector=spec.type=LoadBalancer;
-    echo "\n-------\n";
+    blue "-------";
     k --context kind-pasta-2 get po -A --field-selector=status.phase!=Succeeded; echo
     k --context kind-pasta-2 get svc -A --field-selector=spec.type=LoadBalancer;
 }
@@ -23,8 +28,8 @@ function mlw {
   watch '
     k --context kind-mnger-1 get po -A --field-selector=status.phase!=Succeeded; echo
     k --context kind-mnger-1 get svc -A --field-selector=spec.type=LoadBalancer;
-    echo "\n-------\n";
-    k --context kind-pasta-1 get po -A --field-selector=status.phase!=Succeeded; echo
-    k --context kind-pasta-1 get svc -A --field-selector=spec.type=LoadBalancer;
+    blue "-------";
+    k --context kind-pizza-1 get po -A --field-selector=status.phase!=Succeeded; echo
+    k --context kind-pizza-1 get svc -A --field-selector=spec.type=LoadBalancer;
   '
 }
