64d69d6ad3a38ff04c7cb52f0837abfb5f45586d
Marc Villacorta <h0tbird@github.com>
2024-12-24 11:39:55 +0100

    Update ml

--- functions.sh
+++ functions.sh
@@ -12,13 +12,13 @@ function ml {
     blue "--[ Pizza \xf0\x9f\x8d\x95 ]-------------------------------------------------------------------";
     k --context kind-pizza-1 get po -A --field-selector=status.phase!=Succeeded; echo
     k --context kind-pizza-1 get svc -A --field-selector=spec.type=LoadBalancer;
-    blue "-\xf0\x9f\x91\x86-pizza-1----------\xf0\x9f\x91\x87-pizza-2---------";
+    blue "    -----(pizza-1 \xf0\x9f\x91\x86)----------(pizza-2 \xf0\x9f\x91\x87)-----";
     k --context kind-pizza-2 get po -A --field-selector=status.phase!=Succeeded; echo
     k --context kind-pizza-2 get svc -A --field-selector=spec.type=LoadBalancer;
     blue "--[ Pasta \xf0\x9f\x8d\x9d ]-------------------------------------------------------------------";
     k --context kind-pasta-1 get po -A --field-selector=status.phase!=Succeeded; echo
     k --context kind-pasta-1 get svc -A --field-selector=spec.type=LoadBalancer;
-    blue "-\xf0\x9f\x91\x86-pasta-1----------\xf0\x9f\x91\x87-pasta-2---------";
+    blue "    -----(pasta-1 \xf0\x9f\x91\x86)----------(pasta-2 \xf0\x9f\x91\x87)-----";
     k --context kind-pasta-2 get po -A --field-selector=status.phase!=Succeeded; echo
     k --context kind-pasta-2 get svc -A --field-selector=spec.type=LoadBalancer;
 }
