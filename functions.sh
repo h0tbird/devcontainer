4e96ee9b1afdf2175373acdbc892715d10db149e
Marc Villacorta <h0tbird@github.com>
2024-12-24 10:55:35 +0100

    Update functions

--- functions.sh
+++ functions.sh
@@ -1,15 +1,30 @@
 
-# MeshLab Watch
-function mlw {
-  watch '
-    k --context kind-kube-00 get po -A --field-selector=status.phase!=Succeeded; echo
-    k --context kind-kube-00 get svc -A --field-selector=spec.type=LoadBalancer;
+# MeshLab
+function ml {
+    echo "\n--[ Manager ]---------------------------------------------------\n";
+    k --context kind-mnger-1 get po -A --field-selector=status.phase!=Succeeded; echo
+    k --context kind-mnger-1 get svc -A --field-selector=spec.type=LoadBalancer;
+    echo "\n--[ Pizza ]-----------------------------------------------------\n";
+    k --context kind-pizza-1 get po -A --field-selector=status.phase!=Succeeded; echo
+    k --context kind-pizza-1 get svc -A --field-selector=spec.type=LoadBalancer;
     echo "\n-------\n";
+    k --context kind-pizza-2 get po -A --field-selector=status.phase!=Succeeded; echo
+    k --context kind-pizza-2 get svc -A --field-selector=spec.type=LoadBalancer;
+    echo "\n--[ Pasta ]-----------------------------------------------------\n";
     k --context kind-pasta-1 get po -A --field-selector=status.phase!=Succeeded; echo
     k --context kind-pasta-1 get svc -A --field-selector=spec.type=LoadBalancer;
     echo "\n-------\n";
     k --context kind-pasta-2 get po -A --field-selector=status.phase!=Succeeded; echo
     k --context kind-pasta-2 get svc -A --field-selector=spec.type=LoadBalancer;
-  '
 }
 
+# MeshLab Watch
+function mlw {
+  watch '
+    k --context kind-mnger-1 get po -A --field-selector=status.phase!=Succeeded; echo
+    k --context kind-mnger-1 get svc -A --field-selector=spec.type=LoadBalancer;
+    echo "\n-------\n";
+    k --context kind-pasta-1 get po -A --field-selector=status.phase!=Succeeded; echo
+    k --context kind-pasta-1 get svc -A --field-selector=spec.type=LoadBalancer;
+  '
+}
